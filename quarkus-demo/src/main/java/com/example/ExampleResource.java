fa32df1ccb051d86543d59435688872f95388c54
sukang00 <sukang08109046@163.com>
2022-10-25 16:17:03 +0800

    quarkus-demo 接口创建便利 热更新

---
+++ quarkus-demo/src/main/java/com/example/ExampleResource.java
@@ -0,0 +1,23 @@
+package com.example;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/hello")
+public class ExampleResource {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        return "Hello from RESTEasy Reactive";
+    }
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("/my")
+    public String helloMe() {
+        return "Hello 123 my RESTEasy Reactive";
+    }
+}
