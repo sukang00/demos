c01f37c95d445bdfc89c6ea5e2b73fe05ad22764
sukang00 <sukang08109046@163.com>
2022-10-26 14:54:50 +0800

    quarkus的server调用方式

--- quarkus-demo/src/main/java/com/example/ExampleResource.java
+++ quarkus-demo/src/main/java/com/example/ExampleResource.java
@@ -1,5 +1,8 @@
 package com.example;
 
+import com.example.service.ExampleService;
+
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -8,6 +11,8 @@ import javax.ws.rs.core.MediaType;
 @Path("/hello")
 public class ExampleResource {
 
+    @Inject
+    ExampleService exampleService;
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
@@ -19,5 +24,12 @@ public class ExampleResource {
     @Path("/my")
     public String helloMe() {
         return "Hello 123 my RESTEasy Reactive";
+    }
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("/searchName/{name}")
+    public String searchName(String name) {
+        return exampleService.searchName(name);
     }
 }
---
+++ quarkus-demo/src/main/java/com/example/service/ExampleService.java
@@ -0,0 +1,22 @@
+package com.example.service;
+
+import javax.enterprise.context.ApplicationScoped;
+
+/**
+ * @author sukang
+ * @version 1.0
+ * @description: TODO
+ * @date 2022/10/26 14:50
+ */
+@ApplicationScoped
+public class ExampleService {
+
+    /**
+     *
+     * @param name
+     * @return
+     */
+    public String searchName(String name) {
+        return "hello is " + name;
+    }
+}
