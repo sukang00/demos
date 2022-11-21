6063b30e2cc81d0f528321e95326dfc3183e8b10
sukang00 <sukang08109046@163.com>
2022-11-21 16:26:22 +0800

    kotlin 区间 amis模板

---
+++ kotlin_jvm/src/main/kotlin/RangeToTest.kt
@@ -0,0 +1,17 @@
+fun main(args: Array<String>) {
+    print("循环输出：")
+    for (i in 1..4) print(i) // 输出“1234”
+    println("\n----------------")
+    print("设置步长：")
+    for (i in 1..4 step 2) print(i) // 输出“13”
+    println("\n----------------")
+    print("使用 downTo：")
+    for (i in 4 downTo 1 step 2) print(i) // 输出“42”
+    println("\n----------------")
+    print("使用 until：")
+    // 使用 until 函数排除结束元素
+    for (i in 1 until 4) {   // i in [1, 4) 排除了 4
+        print(i)
+    }
+    println("\n----------------")
+}
