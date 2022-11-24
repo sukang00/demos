c7ce42b1c1e725ab3ce493d84aa869388ad654ba
sukang00 <sukang08109046@163.com>
2022-11-24 16:25:38 +0800

    kotlin 循环和类

---
+++ kotlin_jvm/src/main/kotlin/TestClass.kt
@@ -0,0 +1,62 @@
+class Person {
+
+    var lastName: String = "zhang"
+        get() = field.uppercase() // 将变量赋值后转换为大写
+        set
+
+    var no: Int = 100
+        get() = field                // 后端变量
+        set(value) {
+            if (value < 10) {       // 如果传入的值小于 10 返回该值
+                field = value
+            } else {
+                field = -1         // 如果传入的值大于等于 10 返回 -1
+            }
+        }
+
+    var heiht: Float = 145.4f
+        private set
+}
+class Runoob  constructor(name: String) {  // 类名为 Runoob
+    // 大括号内是类体构成
+    var url: String = "http://www.runoob.com"
+    var country: String = "CN"
+    var siteName = name
+
+    init {
+        println("初始化网站名: ${name}")
+    }
+    // 次构造函数
+    constructor (name: String, alexa: Int) : this(name) {
+        println("Alexa 排名 $alexa")
+    }
+    fun printTest() {
+        println("我是类的函数")
+    }
+}
+
+// 测试
+fun main(args: Array<String>) {
+    var person: Person = Person()
+
+    person.lastName = "wang"
+
+    println("lastName:${person.lastName}")
+
+    person.no = 9
+    println("no:${person.no}")
+
+    person.no = 20
+    println("no:${person.no}")
+    val runoob =  Runoob("菜鸟教程")
+    println(runoob.siteName)
+    println(runoob.url)
+    println(runoob.country)
+    runoob.printTest()
+
+    val runoob2 =  Runoob("菜鸟教程", 10000)
+    println(runoob2.siteName)
+    println(runoob2.url)
+    println(runoob2.country)
+    runoob2.printTest()
+}
---
+++ kotlin_jvm/src/main/kotlin/TestIterator.kt
@@ -0,0 +1,32 @@
+fun main(args: Array<String>) {
+    val items = listOf("apple", "banana", "kiwi")
+    for (item in items) {
+        println(item)
+    }
+
+    for (index in items.indices) {
+        println("item at $index is ${items[index]}")
+    }
+    testWhile()
+    testBreak()
+}
+fun testWhile(){
+    println("----while 使用-----")
+    var x = 5
+    while (x > 0) {
+        println( x--)
+    }
+    println("----do...while 使用-----")
+    var y = 5
+    do {
+        println(y--)
+    } while(y>0)
+}
+fun testBreak(){
+    println("----返回和跳转 使用-----")
+    for (i in 1..10) {
+        if (i==3) continue  // i 为 3 时跳过当前循环，继续下一次循环
+        println(i)
+        if (i>5) break   // i 为 6 时 跳出循环
+    }
+}
