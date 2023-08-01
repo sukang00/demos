a25ff4f0344968e522a3d4d30301754f62fe71c1
sukang00 <sukang08109046@163.com>
2023-08-01 18:13:50 +0800

    socket

---
+++ spring-socket/spring-socket-server/src/main/java/com/socket/server/WebSocketConfig.java
@@ -0,0 +1,20 @@
+package com.socket.server;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+
+/**
+ * @Author: sukang
+ * @CreateTime: 2023-08-01
+ * @Description: TODO
+ * @Version: 1.0
+ */
+@Component
+public class WebSocketConfig {
+
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter() {
+        return new ServerEndpointExporter();
+    }
+}
