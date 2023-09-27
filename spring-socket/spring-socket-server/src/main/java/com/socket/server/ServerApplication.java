96128debc9d4fe559ee57bf9534750ccb4de6479
sukang00 <sukang08109046@163.com>
2023-09-27 08:48:20 +0800

    定时发送消息

--- spring-socket/spring-socket-server/src/main/java/com/socket/server/ServerApplication.java
+++ spring-socket/spring-socket-server/src/main/java/com/socket/server/ServerApplication.java
@@ -1,7 +1,13 @@
 package com.socket.server;
 
+import com.alibaba.fastjson.JSONObject;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import javax.websocket.Session;
 
 /**
  * @Author: sukang
@@ -10,9 +16,23 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @Version: 1.0
  */
 @SpringBootApplication
+@EnableScheduling
+@Slf4j
 public class ServerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ServerApplication.class, args);
     }
+    @Scheduled(cron = "*/5 * * * * ?")
+    public void sendHeartBeat() {
+        log.info("给客户端发送消息：{}",SocketManager.SESSION_POOL.size());
+       if (SocketManager.SESSION_POOL.size()> 0){
+           for (Session session : SocketManager.SESSION_POOL.values()) {
+               JSONObject jsonObject = new JSONObject();
+               jsonObject.put("sessionId",session.getId());
+               session.getAsyncRemote().sendText(jsonObject.toJSONString());
+           }
+       }
+    }
+
 }
